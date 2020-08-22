d99505e703fba1869e46333eb02c13d4875c1546
Lcfvs <lcf.vs@hotmail.com>
2020-08-22 18:14:40 +0200

    Improving docs

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
 /**
  * @module collection
  */
+/**
+ * @type {module:collection/collection}
+ */
 export { default as collection } from './lib/collection.js'
+/**
+ * @type {module:collection/document}
+ */
 export { default as document } from './lib/document.js'
+/**
+ * @type {module:collection/detail}
+ */
 export { default as detail } from './lib/detail.js'
+/**
+ * @type {module:collection/error}
+ */
 export { default as error } from './lib/error.js'
+/**
+ * @type {module:collection/item}
+ */
 export { default as item } from './lib/item.js'
+/**
+ * @type {module:collection/link}
+ */
 export { default as link } from './lib/link.js'
+/**
+ * @type {module:collection/property}
+ */
 export { default as property } from './lib/property.js'
+/**
+ * @type {module:collection/query}
+ */
 export { default as query } from './lib/query.js'
+/**
+ * @type {module:collection/script}
+ */
 export { default as script } from './lib/script.js'
+/**
+ * @type {module:collection/template}
+ */
 export { default as template } from './lib/template.js'
